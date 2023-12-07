b11489cf92173c228cffa0fcbb9666d46005f1b2
anu1shag <145717240+anu1shag@users.noreply.github.com>
2023-12-06 20:37:13 -0500

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,20 +1,15 @@
 import random
+import os
+
 def read_word_list(file_path):
     with open(file_path, 'r') as file:
         words = file.read().splitlines()
     return words
-import os
 
-#reading file
-file_name = "wordList.txt"
-file_path = os.path.join(os.getcwd(), file_name)
-word_list = read_word_list(file_path)
-
-#choosing a word
 def choose_word(word_list):
     return random.choice(word_list)
 
-def display_word(secret_word, guessed_word):
+def display_word(secret_word, guessed_word, guessed_letters):
     displayed_word = ""
     for i in range(len(secret_word)):
         if i < len(guessed_word) and guessed_word[i] == secret_word[i]:
@@ -23,7 +18,12 @@ def display_word(secret_word, guessed_word):
             displayed_word += f"{guessed_word[i].upper()} "
         else:
             displayed_word += "_ "
+
+    remaining_letters = [letter.upper() if letter not in guessed_letters else ' ' for letter in 'abcdefghijklmnopqrstuvwxyz']
+    print(" ".join(remaining_letters))
+    
     return displayed_word
+
 
 def play_wordle():
     max_attempts = 6
@@ -32,17 +32,25 @@ def play_wordle():
     word_list = read_word_list(file_path)
     secret_word = choose_word(word_list)
 
-    print("Welcome to Wordle!")
-    print("Can you guess the word?")
-    print(display_word(secret_word, ""))
+    print("Welcome to Wordle! You will have six attempts to guess a five-letter word.")
+    print("If a letter is uppercase, then it is in the wrong place.")
+    print("If a letter is lowercase, then it is correct.")
+    print("Ready to play?")
+    
+    guessed_letters = []  # Initialize the guessed letters list
+    print(display_word(secret_word, "", guessed_letters))
 
     attempts = 0
     while attempts < max_attempts:
         guess = input("Enter your guess: ").lower()
 
         if len(guess) != len(secret_word) or not guess.isalpha():
-            print("That is an invalid input. Please try again")
+            print("Invalid input. Please enter a guess with the correct length.")
             continue
+
+        guessed_letters.extend(guess)
+        current_display = display_word(secret_word, guess, guessed_letters)
+        print(current_display)
 
         if guess == secret_word:
             print("Congrats! You guessed the word:", secret_word)
@@ -50,11 +58,12 @@ def play_wordle():
         else:
             attempts += 1
             remaining_attempts = max_attempts - attempts
-            feedback = display_word(secret_word, guess)
+            feedback = display_word(secret_word, guess, guessed_letters)
             print("Incorrect guess. Attempts remaining:", remaining_attempts)
             print("Feedback:", feedback)
 
     if attempts == max_attempts:
         print("Sorry, you ran out of attempts. The word was:", secret_word)
+
 if __name__ == "__main__":
     play_wordle()
